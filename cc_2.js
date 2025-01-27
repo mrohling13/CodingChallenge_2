1d568af20b30e9d4ca1d2b2336a4bcbf55b25e78
mrohling13 <malachi.rohling@yahoo.com>
2025-01-26 21:58:56 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,14 @@
+// Task 1: Working with Arrays //
+
+let products = ['Shield Cell','Phoenix Kit', 'Med Kit', 'Syringe', 'Thermite']; // Array of products
+
+products.push('Boosted Loader'); // Add new products to the end of the array
+
+console.log(products); // print the array of products
+
+products.pop(); // Remove the last product from the array
+
+console.log('Updated products:',products); // print the array of products 
+
+
+
